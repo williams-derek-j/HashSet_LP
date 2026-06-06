a2a6d5f4c9fd17890647afe716528a654dd8b4bc
Derek J. WIlliams <derek-j-williams@proton.me>
2026-06-05 23:17:50 -0700

    probe distance constructor, possible to have negative probe distance

--- src/HashSet.java
+++ src/HashSet.java
@@ -2,16 +2,30 @@ public class HashSet {
     private int size = 0;
     private final double loadFactor = 0.8;
     private Integer[] elementData = new Integer[16];
-    private final int probeDistance = 1;
+    private final int probeDistance;
     private final Integer TOMBSTONE = Integer.MIN_VALUE;
 
     public HashSet() {
+        probeDistance = 1;
     }
     public HashSet(int initialCapacity) {
         if (initialCapacity > 0) {
             elementData = new Integer[initialCapacity];
         } else {
             throw new IllegalArgumentException("Initial capacity must be > 0");
+        }
+        probeDistance = 1;
+    }
+    public HashSet(int initialCapacity, int probeDistance) {
+        if (initialCapacity > 0) {
+            elementData = new Integer[initialCapacity];
+        } else {
+            throw new IllegalArgumentException("Initial capacity must be > 0");
+        }
+        if (probeDistance != 0) {
+            this.probeDistance = probeDistance;
+        } else {
+            throw new IllegalArgumentException("Probe distance cannot be 0");
         }
     }
 
@@ -20,7 +34,6 @@ public class HashSet {
 
         return contains(value, index);
     }
-
     private int contains(int value, int index) {
         int counter = elementData.length;
 
@@ -28,7 +41,14 @@ public class HashSet {
             if (elementData[index] != TOMBSTONE && elementData[index].equals(value)) {
                 return index;
             }
-            index = (index + probeDistance) % elementData.length;
+            if (probeDistance > 0) {
+                index = (index + probeDistance) % elementData.length;
+            } else {
+                index = index + probeDistance;
+                if (index < 0) {
+                    index = elementData.length + probeDistance;
+                }
+            }
             counter--;
         }
         return -1;
@@ -49,7 +69,17 @@ public class HashSet {
     public boolean add(int toAdd) {
         return add(toAdd, true);
     }
+    public boolean add(int[] toAddArray) {
+        for (int toAdd : toAddArray) {
+            boolean successful = add(toAdd, true);
 
+            if (!successful) {
+                elementData = new Integer[0]; // clear data
+                return false;
+            }
+        }
+        return true;
+    }
     public boolean add(int toAdd, boolean shouldResize) {
         if (shouldResize) {
             if ((double) size / elementData.length >= loadFactor) {
@@ -69,7 +99,15 @@ public class HashSet {
             } else if (elementData[index].equals(toAdd)) {
                 return false;
             }
-            index = (index + probeDistance) % elementData.length;
+
+            if (probeDistance > 0) {
+                index = (index + probeDistance) % elementData.length;
+            } else {
+                index = index + probeDistance;
+                if (index < 0) {
+                    index = elementData.length + probeDistance;
+                }
+            }
             counter--;
         }
         elementData[index] = toAdd;
@@ -107,10 +145,39 @@ public class HashSet {
         return (Math.abs(el) * 13) % elementData.length;
     }
 
+
     public String toString() {
         if (size == 0) {
             return "[]";
         }
+
+        StringBuilder output = new StringBuilder("[");
+
+        for (int i = 0; i < elementData.length; i++) {
+            if (i == 0) {
+                while (elementData[i] == null || elementData[i] == TOMBSTONE) {
+                    i++;
+                }
+                output.append(elementData[i]);
+                i++;
+            }
+            if (elementData[i] != TOMBSTONE && elementData[i] != null) {
+                output.append(", ").append(elementData[i]);
+            }
+        }
+        output.append("]");
+
+        return output.toString();
+    }
+    public String toString(boolean verbose) {
+        if (!verbose) {
+            return toString();
+        }
+
+        if (size == 0) {
+            return "[]";
+        }
+
         StringBuilder output = new StringBuilder("[");
 
         if (elementData[0] == TOMBSTONE) {
